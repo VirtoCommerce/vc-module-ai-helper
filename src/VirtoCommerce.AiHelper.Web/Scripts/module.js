395311c47c3880508233be3083d0f78e100c4b6f
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2025-11-07 14:46:41 +0100

    VM-1662: fixed ai-helper-app manifest

--- src/VirtoCommerce.AiHelper.Web/Scripts/module.js
+++ src/VirtoCommerce.AiHelper.Web/Scripts/module.js
@@ -8,36 +8,36 @@ if (AppDependencies !== undefined) {
 angular.module(moduleName, [])
     .config(['$stateProvider',
         function ($stateProvider) {
-            $stateProvider
-                .state('workspace.AiHelperState', {
-                    url: '/ai-helper',
-                    templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
-                    controller: [
-                        'platformWebApp.bladeNavigationService',
-                        function (bladeNavigationService) {
-                            var newBlade = {
-                                id: 'blade1',
-                                controller: 'virtoCommerce.AiHelper.translateController',
-                                template: 'Modules/$(VirtoCommerce.AiHelper)/Scripts/blades/translate.html',
-                                isClosingDisabled: true,
-                            };
-                            bladeNavigationService.showBlade(newBlade);
-                        }
-                    ]
-                });
+        //    $stateProvider
+        //        .state('workspace.AiHelperState', {
+        //            url: '/ai-helper',
+        //            templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
+        //            controller: [
+        //                'platformWebApp.bladeNavigationService',
+        //                function (bladeNavigationService) {
+        //                    var newBlade = {
+        //                        id: 'blade1',
+        //                        controller: 'virtoCommerce.AiHelper.translateController',
+        //                        template: 'Modules/$(VirtoCommerce.AiHelper)/Scripts/blades/translate.html',
+        //                        isClosingDisabled: true,
+        //                    };
+        //                    bladeNavigationService.showBlade(newBlade);
+        //                }
+        //            ]
+        //        });
         }
     ])
     .run(['platformWebApp.mainMenuService', '$state',
         function (mainMenuService, $state) {
             //Register module in main menu
-            var menuItem = {
-                path: 'browse/ai-helper',
-                icon: 'fa fa-cube',
-                title: 'AiHelper',
-                priority: 100,
-                action: function () { $state.go('workspace.AiHelperState'); },
-                permission: 'ai-helper:access',
-            };
-            mainMenuService.addMenuItem(menuItem);
+        //    var menuItem = {
+        //        path: 'browse/ai-helper',
+        //        icon: 'fa fa-cube',
+        //        title: 'AiHelper',
+        //        priority: 100,
+        //        action: function () { $state.go('workspace.AiHelperState'); },
+        //        permission: 'ai-helper:access',
+        //    };
+        //    mainMenuService.addMenuItem(menuItem);
         }
     ]);
